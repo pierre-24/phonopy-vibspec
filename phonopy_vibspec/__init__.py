7020a95228b57e5b9e1cc9b505c2df51f385b91e
Pierre Beaujean <pierreb24@gmail.com>
2023-10-22 20:50:36 +0200

    Bump version: 0.3.0 → 0.3.1

--- phonopy_vibspec/__init__.py
+++ phonopy_vibspec/__init__.py
@@ -5,7 +5,7 @@ Simulate IR and Raman spectra with Phonopy
 import logging
 import os
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Pierre Beaujean'
 __maintainer__ = 'Pierre Beaujean'
 __email__ = 'pierre.beaujean@unamur.be'
